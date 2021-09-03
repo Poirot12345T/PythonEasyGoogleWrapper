7bd6f707b1815534343c6031a00fe7bfa25c63a4
Poirot12345 T <82270962+Poirot12345T@users.noreply.github.com>
2021-09-03 07:11:40 +0200

    add "logging started at" phrase

--- logger.py
+++ logger.py
@@ -9,7 +9,7 @@ class Logger:
     def __init__(self, app_type, message):
         startdate = get_time().split("T")[0]
         self.logname = f"log{app_type}_" + startdate + ".txt"
-        logmessage = get_time() + ": " + message
+        logmessage = "logging started at " + get_time() + ": " + message
         print(logmessage)
         with open(self.logname, "a") as logfile:
             logfile.write("\n\n" + logmessage)
