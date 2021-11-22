1cea4b740962ab62fafec80af11ab6c83ef723be
Poirot12345 T <82270962+Poirot12345T@users.noreply.github.com>
2021-11-22 14:54:31 +0100

    get_user_albums()

--- photos_service.py
+++ photos_service.py
@@ -4,3 +4,14 @@ class PhotoService(GeneralService):
     def __init__(self, app_type, secret_file):
         super().__init__(app_type, secret_file,'photoslibrary','v1', ['https://www.googleapis.com/auth/photoslibrary', 'https://www.googleapis.com/auth/photoslibrary.sharing'])
         
+    def get_user_albums(self):
+        response = self.communicate.albums().list(pageSize=50, excludeNonAppCreatedData=False).execute()
+        next_page_token = response.get("nextPageToken")
+        albums = response.get("albums")
+        while next_page_token:
+            circular_response = self.communicate.albums().list(pageSize=50, excludeNonAppCreatedData=False, pageToken=next_page_token).execute()
+            whiled_album = circular_response.get("albums")
+            next_page_token = circular_response.get("nextPageToken")
+            if whiled_album:
+                albums.append(whiled_album)
+        return albums
