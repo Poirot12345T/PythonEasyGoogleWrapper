ca7ee9f2d69e0d7055200e4c93af460dff88373c
František Tichý <fanda987654@seznam.cz>
2022-02-24 11:53:28 +0100

    drive service commentary

--- drive_service.py
+++ drive_service.py
@@ -8,7 +8,7 @@ class DriveService(GeneralService):
     def __init__(self, app_type, secret_file, user_mail):
         super().__init__(app_type, secret_file, 'drive', 'v3', user_mail, ["https://www.googleapis.com/auth/drive"])
         
-    def get_mimetype(self, file_name):
+    def get_mimetype(self, file_name: str) -> str:
         """
         returns a MIME type of the file
         """
@@ -23,7 +23,7 @@ class DriveService(GeneralService):
             self.log.log_message(f'{file_name} is unsupported file type')
             raise UnknownFileType
     
-    def search_in_folder(self, id):
+    def search_in_folder(self, id: str) -> dict:
         """
         returns info about all files in folder as a list of dicts, where one dict includes: ID (as 'id' key), name and MIME type (key 'mimeType')
         """
@@ -38,13 +38,22 @@ class DriveService(GeneralService):
             nextPageToken = response.get('nextPageToken')
         return files
 
-    def upload(self, what, where, to): 
+    def upload(self, what: str, where: str, to: str) -> None: 
+        """
+        Uploads a file with name  "what" and path of "where", placing ingo Google's folder of ID "to".
+        """
+
         mime_type = self.get_mimetype(what)
         file_metadata = {"name": what,"parents":[to]}
         media = MediaFileUpload(str(where), mimetype=mime_type)
         file = self.communicate.files().create(body=file_metadata,media_body=media,fields="id").execute()
+        self.log.log_message(f"file {what} successfully uploaded")
     
-    def download(self, what_id, what_name, where):
+    def download(self, what_id: str, what_name: str, where: str) -> None:
+        """
+        Downloads file selected by ID and stores it in a file with "what_name" name and in "where" folder
+        """
+
         request = self.communicate.files().get_media(fileId=what_id)
         fh = io.BytesIO()
         downloader = MediaIoBaseDownload(fd=fh, request=request)
@@ -59,6 +68,15 @@ class DriveService(GeneralService):
         with open(pathlib.Path(where, what_name), "wb") as f:
             f.write(fh.read())
             f.close()
+            self.log.log_message(f"file {what_name} successfully downloaded")
     
-    def delete(self, file_id):
+    def delete(self, file_id: str) -> None:
+        """
+        Removes file with ID put into the function from Google Drive completely
+        No trash recovery available!
+        Be sure this function is somehow secured in your implementation as you have no chance to recover the file
+        TODO: add "are you sure?" dialog
+        """
         self.communicate.files().delete(fileId=file_id).execute()
+        self.log.log_message(f"file ID {file_id} successfully deleted")
+
