420c88290187b04b11f674be9c75d3f9639594d8
František Tichý <fanda987654@seznam.cz>
2023-04-24 21:18:23 +0200

    read spreadsheet

--- sheet_service.py
+++ sheet_service.py
@@ -1,5 +1,16 @@
 from general_service import GeneralService
 
 class SheetService(GeneralService):
-    def __init__(self, app_type, secret_file):
-        super().__init__(app_type, secret_file, 'sheets', 'v4', ['https://www.googleapis.com/auth/spreadsheets'])
+    def __init__(self, app_type, secret_file, user_mail):
+        super().__init__(app_type, secret_file, 'sheets', 'v4', user_mail, ['https://www.googleapis.com/auth/spreadsheets'])
+        self.sheet = self.communicate.spreadsheets()
+
+    def get_spreadsheet_data(self, spreadsheet_id: str, list_name: str, begin_cell: str, end_cell: str) -> list:
+        """
+        Returns 2D array from selected range of spreadsheet
+        """
+        range_def = f"{list_name}!{begin_cell}:{end_cell}"
+        call_result = self.sheet.values().get(spreadsheetId=spreadsheet_id, range=range_def).execute()
+        return call_result.get('values',[])
+
+        
