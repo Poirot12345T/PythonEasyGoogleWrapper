fadd9f214c107641ef44f977e7543013593c6aed
František Tichý <fanda987654@seznam.cz>
2024-02-17 12:20:46 +0100

    rename function

--- sheet_service.py
+++ sheet_service.py
@@ -5,7 +5,7 @@ class SheetService(GeneralService):
         super().__init__(app_type, secret_file, 'sheets', 'v4', user_mail, ['https://www.googleapis.com/auth/spreadsheets'])
         self.sheet = self.communicate.spreadsheets()
 
-    def get_spreadsheet_data(self, spreadsheet_id: str, list_name: str, begin_cell: str, end_cell: str) -> list:
+    def get_selected_data(self, spreadsheet_id: str, list_name: str, begin_cell: str, end_cell: str) -> list:
         """
         Returns 2D array from selected range of spreadsheet
         """
